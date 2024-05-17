139bf4a6b2ec0132fe43fccb9aa0c6d4b8da8920
radulovskia <aleksandar.radulovski15@gmail.com>
2024-05-17 19:50:29 +0200

    Make consumer async

--- backend_analytics/main.py
+++ backend_analytics/main.py
@@ -1,30 +1,30 @@
 from fastapi import FastAPI, WebSocket
-from confluent_kafka import Consumer
+from aiokafka import AIOKafkaConsumer
 import json
-import asyncio
 
 app = FastAPI()
 
 kafka_topic = "survey_topic"
-conf = {
-    # "bootstrap.servers": "localhost:9092,localhost:9094,localhost:9096", # external
-    "bootstrap.servers": "broker1:9091,broker2:9093,broker3:9095", # internal
-    "group.id": "survey-analytics",
-    "session.timeout.ms": 6000,
-    "auto.offset.reset": "earliest",
-    "enable.auto.offset.store": False,
-}
+# kafka_servers = "localhost:9092,localhost:9094,localhost:9096" # external
+kafka_servers = "broker1:9091,broker2:9093,broker3:9095" # internal
+group_id = "survey-analytics"
 
-consumer = Consumer(conf)
-consumer.subscribe([kafka_topic])
-
-async def kafka_poll(websocket: WebSocket):
-    msg = consumer.poll(1.0)
-    if msg is not None:
-        if msg.error():
-            print("Consumer error: {}".format(msg.error()))
-        msg_value = msg.value().decode("utf-8")
-        await send_to_websocket(msg_value, websocket)
+async def kafka_polling(websocket):
+    consumer = AIOKafkaConsumer(
+        kafka_topic,
+        bootstrap_servers=kafka_servers,
+        auto_commit_interval_ms=1000,
+        auto_offset_reset='earliest'
+    )
+    await consumer.start()
+    try:
+        async for msg in consumer:
+            msg_value = msg.value.decode("utf-8")
+            await send_to_websocket(msg_value, websocket)
+    except Exception as e:
+        print(f"Kafka consumer error: {e}")
+    finally:
+        await consumer.stop()
 
 async def send_to_websocket(msg, websocket):
     msg_json = json.dumps(msg)
@@ -34,7 +34,6 @@ async def send_to_websocket(msg, websocket):
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
     try:
-        asyncio.create_task(kafka_poll(websocket))
-        await asyncio.sleep(1)
+        await kafka_polling(websocket)
     except Exception as e:
         print(f"WebSocket connection closed: {e}")
